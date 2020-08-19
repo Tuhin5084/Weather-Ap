d9063366e4f2711de18154fd1da74b53cf4b4c93
Tuhin5084 <touhiduzzamantuhin95@gmail.com>
2020-08-20 01:48:02 +0600

    Weather App Done

---
+++ weather.js
@@ -0,0 +1,30 @@
+document.getElementById('searchButton').addEventListener('click', function(){
+    const city = document.getElementById('cityName').value;
+    document.getElementById('cityName').value = '';
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b8545c8cd1a0cf6b38c99ab322c34fc4&units=metric`)
+    .then(res => res.json())
+    .then(json => displayWeatherDetails(json))
+})
+
+function displayWeatherDetails(value){
+    console.log(value)
+    document.getElementById('cityChange').innerHTML = `${value.name} , ${value.sys.country}`;
+    document.getElementById('description').innerHTML = `${value.weather[0].main}`;
+    document.getElementById('iconWeather').src = `https://openweathermap.org/img/wn/${value.weather[0].icon}.png`;
+    document.getElementById('tempValue').innerHTML = `${value.main.temp} &deg;C`;
+    document.getElementById('humidity').innerHTML = `Humidity : ${value.main.humidity} %`;
+    document.getElementById('wind').innerHTML = `Wind Speed : ${value.wind.speed} m/s`;
+    document.getElementById('sunsetTime').innerHTML = ` Sunset Time : ${(new Date(value.sys.sunset)).toLocaleTimeString()}`;
+    
+    const options = {
+        weekday : 'long',
+        day : 'numeric',
+        month : 'long'
+    }
+    const currentDate = new Date();
+    const myDate = currentDate.toDateString(options);
+
+    document.getElementById('date').innerHTML = myDate;
+
+}
+ 
